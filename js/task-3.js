e5298f278971ac241e40805f026618f655dfe8db
alexdevsprint <alexdevsprint@gmail.com>
2025-02-06 16:55:31 +0200

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
 function inputText(event) {
   event.currentTarget.value = event.currentTarget.value.trim();
   if (event.currentTarget.value) {
-    document.querySelector('#name-output').innerHTML =
+    document.querySelector('#name-output').textContent  =
       event.currentTarget.value;
   } else {
-    document.querySelector('#name-output').innerHTML = 'Anonymous';
+    document.querySelector('#name-output').textContent  = 'Anonymous';
   }
 }
 document.querySelector('#name-input').addEventListener('input', inputText);
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+function sendForm(event) {
+  event.preventDefault(); 
+  const email = document.querySelector('input[name="email"]').value.trim();
+  const password = document.querySelector('input[name="password"]').value.trim();  
+  if (email && password) {
+
+  } else {
+    alert('All form fields must be filled in');
+  }
+}
+
+
+
+
+document.querySelector('.login-form').addEventListener('submit', sendForm);
+
 
