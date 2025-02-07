fe1cfc9203ac829d7e8a810c8744d605415c7f31
alexdevsprint <alexdevsprint@gmail.com>
2025-02-07 16:21:43 +0200

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,34 @@
-function sendForm(event) {
-  event.preventDefault(); 
-  const email = document.querySelector('input[name="email"]').value.trim();
-  const password = document.querySelector('input[name="password"]').value.trim();  
-  if (email && password) {
+function validateForm(form) {  
+  for (let element of form.elements) {
+    if (element.name && element.value.trim() === '') {
+      
+      return false;
+    }
+    
+  }
+  return true;
+}
 
+function getFormData(form) {
+  const formData = {};
+  for (let element of form.elements) {
+    if (element.name) {
+      formData[element.name] = element.value.trim();
+    }
+  }
+  return formData;
+}
+
+function sendForm(event) {
+  event.preventDefault();
+  const form = document.querySelector('.login-form');
+
+  if (validateForm(form)) {
+    console.log(getFormData(form));
+    form.reset();
   } else {
     alert('All form fields must be filled in');
   }
 }
 
-
-
-
 document.querySelector('.login-form').addEventListener('submit', sendForm);
-
-
